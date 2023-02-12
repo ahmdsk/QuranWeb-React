ceb0a0afb291cbe4bbaa324f15757ce2336715be
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-12 23:14:10 +0700

    fetch data from api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 export default function Home() {
+    const [surah, setSurah] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function getAllSurah() {
+            const request = await fetch('https://equran.id/api/surat');
+            const response = await request.json();
+
+            setSurah(response)
+            setLoading(false)
+        }
+
+        getAllSurah()
+    }, []);
+
     return (
         <>
             <div className="topBar">
                 <span>
                     <img src="/menu-2-line.svg" alt="" />
                 </span>
-                <div className="title-home">Quran App</div>
+                <div className="title-home">
+                    <Link to="/">Quran App</Link>
+                </div>
                 <span>
                     <img src="/search-line.svg" alt="" />
                 </span>
             </div>
+
             <div className="banner">
                 <div className="left-info">
                     <div className="container-last-read">
@@ -27,6 +47,29 @@ export default function Home() {
                     <img src="/quran.svg" alt="Quran" />
                 </div>
             </div>
+
+            {loading ? (<span className="loading">Loading Content...</span>) : (
+                <div className="list-surah">
+                    {surah.map((s) => {
+                        return (
+                            <div className="surah" key={s.nomor}>
+                                <div className="surah-title">
+                                    <div className="no-surah">
+                                        <span>{s.nomor}</span>
+                                    </div>
+                                    <div className="surah-location">
+                                        <h2>{s.nama_latin}</h2>
+                                        <span>{s.tempat_turun} - {s.jumlah_ayat} Ayat</span>
+                                    </div>
+                                </div>
+                                <div className="name-surah-latin">
+                                    <h2>{s.nama}</h2>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
         </>
     )
 }
