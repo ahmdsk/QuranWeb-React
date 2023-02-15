2cf344f5504ff248a56c8497ff4e575c72acdba5
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-15 16:32:51 +0700

    update anchor tag error in netlify

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -75,24 +75,24 @@ export default function Home() {
                         <ul>
                             <li>
                                 <AiOutlineCoffee />
-                                <a href="#">Trakteer coffe</a>
+                                <a href="/#">Trakteer coffe</a>
                             </li>
                             <li>
                                 <AiOutlineBug />
-                                <a href="#"> Report bug</a>
+                                <a href="/#"> Report bug</a>
                             </li>
                             <li>
                                 <AiOutlineFolderOpen />
-                                <a href="#">This Repository</a>
+                                <a href="/#">This Repository</a>
                             </li>
                         </ul>
                     </div>
                     <div className="sidebar-footer">
                         <h3 className="footer-title">Contact me</h3>
                         <div className="icons-social">
-                            <a href="https://www.github.com/ahmdsk" target="_blank" rel="noopener noreferrer"><AiOutlineGithub /></a>
-                            <a href="https://www.wa.me/62895605997185" target="_blank" rel="noopener noreferrer"><AiOutlineWhatsApp /></a>
-                            <a href="https://www.instagram.com/ahmdsk._" target="_blank" rel="noopener noreferrer"><AiOutlineInstagram /></a>
+                            <a href="https://github.com/ahmdsk" target="_blank" rel="noopener noreferrer"><AiOutlineGithub /></a>
+                            <a href="https://wa.me/62895605997185" target="_blank" rel="noopener noreferrer"><AiOutlineWhatsApp /></a>
+                            <a href="https://instagram.com/ahmdsk._" target="_blank" rel="noopener noreferrer"><AiOutlineInstagram /></a>
                         </div>
                         <h4 className="copyright">Developed by <b>Ahmad Shaleh</b></h4>
                     </div>
